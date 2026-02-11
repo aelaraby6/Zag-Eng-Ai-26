eaa7b5efce0cb18ce5ca614cd56443669ea27d4f
aelaraby6 <body1262005@gmail.com>
2026-02-11 19:54:30 +0200

    add pandas task

--- Tasks/pandas/LeetCode_Pandas/Method Chaining.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-
-def findHeavyAnimals(animals: pd.DataFrame) -> pd.DataFrame:
-    heavy_animals = animals[animals['weight'] > 100]
-    return heavy_animals.sort_values(by='weight', ascending=False)[['name']]
